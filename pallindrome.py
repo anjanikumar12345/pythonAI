77351b22c0e20ae573f6a37b38edbcf5cd8b01d9
anjanikumar12345 <v.anjanikumar333@gmail.com>
2025-03-10 13:17:20 +0530

    OOO

--- pallindrome.py
+++ pallindrome.py
@@ -1,9 +1,6 @@
 
+
+
 a=int(input("Enter the value:   "))
 rev_num=int(str(a)[::-1])
 print(rev_num)
-
-if a==rev_num:
-    print("YES IT IS Palindrome !!!!!!")
-else:
-    print("NO IT is not a PAllindrone !")
